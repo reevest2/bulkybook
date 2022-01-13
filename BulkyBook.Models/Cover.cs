d608fa7dde2c0d78f57105e84a50e95c89c30c8f
Trevor Reeves <reevest2@gmail.com>
2022-01-13 17:31:27 -0500

    added max length

--- BulkyBook.Models/Cover.cs
+++ BulkyBook.Models/Cover.cs
@@ -13,6 +13,7 @@ namespace BulkyBook.Models
         public int Id { get; set; }
         [Required]
         [Display(Name = "Cover Type")]
+        [MaxLength(50)]
         public string Name { get; set; } 
     }
 }
