c8916bfc31d37ca23c179caffb4741b9ee52dcb3
Trevor Reeves <reevest2@gmail.com>
2022-01-13 17:23:04 -0500

    Display name = Cover type

--- BulkyBook.Models/Cover.cs
+++ BulkyBook.Models/Cover.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Cover Type")]
         public string Name { get; set; } 
     }
 }
