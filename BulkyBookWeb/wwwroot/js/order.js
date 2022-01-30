1c580481aba2c0e997e5ec9fb2cce37371156025
Trevor Reeves <reevest2@gmail.com>
2022-01-30 03:30:00 -0500

    order status page created

---
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -0,0 +1,33 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll"
+        },
+        "columns": [
+            { "title": "ID", "data": "id", "width": "5%" },
+            { "title": "Name", "data": "name", "width": "25%" },
+            { "title": "Phone Number", "data": "phoneNumber", "width": "15%" },
+            { "title": "Email", "data": "applicationUser.email", "width": "15%" },
+            { "title": "Order Status", "data": "orderStatus", "width": "15%" },
+            { "title": "Order Total", "data": "orderTotal", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Order/Details?orderId=${data}"
+                            class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
+                            </div>
+                        `
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
